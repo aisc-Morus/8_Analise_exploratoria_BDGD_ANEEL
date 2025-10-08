46d873ebeba2c70960a9503ef3ae3b5f6948ba66
Andre Carmona <a.i.s.cargmona@gmail.com>
2025-10-08 10:01:09 -0300

    Commit 1
    Monta script base

--- base.R
+++ base.R
@@ -3,13 +3,40 @@
 if (!require(pacman)) install.packages("pacman")
 pacman::p_load(tidyverse, readxl, sf, purr)
 
-# 2. Monta df ----
+# 2. Camadas BDGD ----
 
 bdgd <- list.files(pattern = ".gdb") %>% 
         as.list
 camadas <- map(bdgd, st_layers)
 camadas
 
+# 3. Monta shapefile ----
+df_shape <-  map(bdgd, \(f) {ano <- stringr::str_extract(f, "20[0-9]{2}")
+                             lay <- st_layers(f)$name
+                             lay_filtrado <- "PONNOT"
+                             map(lay_filtrado, \(l) {st_read(f, layer = l) %>%
+                                                     mutate(ano = ano, 
+                                                            camada = l) %>% 
+                                                     rename_with(tolower)
+                                                    })
+                            }) %>% 
+             flatten() %>% 
+             bind_rows() %>% 
+             select(cod_id, shape) %>%  
+             distinct() %>% 
+             mutate(lon = st_coordinates(shape)[, 1],
+                    lat = st_coordinates(shape)[, 2])
+df_shape %>% st_crs
+
+# 4. Projeta shapefile usando mapa interativo TMP
+
+tm_shape(df_shape) +
+  tm_dots(col = "red", size = 0.2) +
+  tm_basemap("OpenStreetMap") +
+  tm_layout(title = "PONNOT/BDGD - EFLUL/SC ")
+
+
+# 4. Monta df ----
 df <- map(bdgd, \(f) {ano <- stringr::str_extract(f, "20[0-9]{2}")
                       lay <- st_layers(f)$name
                       lay_filtrado <- lay %>%
@@ -23,31 +50,35 @@ df <- map(bdgd, \(f) {ano <- stringr::str_extract(f, "20[0-9]{2}")
                      }) %>%
       flatten() %>% 
       bind_rows %>% 
-           select(distribuidora = dist, 
-                  id            = cod_id, 
-                  livre         = liv, 
-                  tarifa        = gru_tar, 
-                  cnae,
-                  classe_bdgd   = clas_sub,
-                  cep, 
-                  municipio     = mun,
-                  ano,
-                  camada,
-                  starts_with("ene"))
+      select(distribuidora = dist, 
+             id            = cod_id, 
+             livre         = liv, 
+             tarifa        = gru_tar, 
+             cnae,
+             classe_bdgd   = clas_sub,
+             cep, 
+             municipio     = mun,
+             ano,
+             camada,
+             starts_with("ene")) %>% 
+      left_join(df_sha)
 
-df_shape <-  map(bdgd, \(f) {ano <- stringr::str_extract(f, "20[0-9]{2}")
-                             lay <- st_layers(f)$name
-                             lay_filtrado <- "PONNOT"
-                             map(lay_filtrado, \(l) {st_read(f, layer = l) %>%
-                                                     mutate(ano = ano, 
-                                                     camada = l) %>% 
-                                                     rename_with(tolower)
-                                                     })
-                            }) %>% 
-             flatten() %>% 
-             bind_rows() %>% 
-             select(cod_id, shape) %>%  distinct()
+
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                                       
 
 
