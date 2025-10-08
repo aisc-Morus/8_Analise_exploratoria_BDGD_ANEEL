50554ecd0f5e340fbe01a590592ee31d566b817e
Andre Carmona <a.i.s.cargmona@gmail.com>
2025-10-08 14:19:28 -0300

    - incluido extração do shapefile e projeção em mapa interativo (tmap) - incluido montagem parcial do df

--- base.R
+++ base.R
@@ -1,7 +1,7 @@
 
 # 1. Prepara ambiente ----
 if (!require(pacman)) install.packages("pacman")
-pacman::p_load(tidyverse, readxl, sf, purr)
+pacman::p_load(tidyverse, readxl, sf, tmap)
 
 # 2. Camadas BDGD ----
 
@@ -22,17 +22,19 @@ df_shape <-  map(bdgd, \(f) {ano <- stringr::str_extract(f, "20[0-9]{2}")
                             }) %>% 
              flatten() %>% 
              bind_rows() %>% 
-             select(cod_id, shape) %>%  
+             select(id = cod_id, shape) %>%  
              distinct() %>% 
              mutate(lon = st_coordinates(shape)[, 1],
                     lat = st_coordinates(shape)[, 2])
 df_shape %>% st_crs
+df_shape %>% str
 
-# 4. Projeta shapefile usando mapa interativo TMP
+# 4. Projeta shapefile usando mapa interativo TMAP
 
+tmap_mode("view")
 tm_shape(df_shape) +
-  tm_dots(col = "red", size = 0.2) +
-  tm_basemap("OpenStreetMap") +
+  tm_dots(col = "blue", size = 0.15) +
+  tm_basemap("OpenStreetMap") +    
   tm_layout(title = "PONNOT/BDGD - EFLUL/SC ")
 
 
@@ -51,27 +53,91 @@ df <- map(bdgd, \(f) {ano <- stringr::str_extract(f, "20[0-9]{2}")
       flatten() %>% 
       bind_rows %>% 
       select(distribuidora = dist, 
-             id            = cod_id, 
+             id            = pn_con, 
              livre         = liv, 
-             tarifa        = gru_tar, 
+             gru_tar, 
              cnae,
              classe_bdgd   = clas_sub,
              cep, 
              municipio     = mun,
              ano,
              camada,
-             starts_with("ene")) 
+             starts_with("ene")) %>% 
+       left_join(df_shape, 
+                 by ="id", 
+                 multiple = "first") %>% 
+       select(-matches("^ene(_p|_f)")) %>%
+       pivot_longer(cols = starts_with("ene"),
+                    names_to = "mes",
+                    values_to = "kwh" ) %>%
+       mutate(referencia = as.Date(paste0(ano, 
+                                          "-", 
+                                          str_extract(mes, "\\d{2}"), "-01")),
+              cnae       = str_remove_all(cnae, "[^0-9]")) %>% 
+       distinct() %>% 
+       pivot_wider(id_cols     = c(distribuidora, id, livre, gru_tar, cnae, classe_bdgd, cep, municipio, camada, shape, lon, lat),
+                   names_from  = referencia,         
+                   values_from = kwh)
+
+
+df %>% str
+
+      
+
+
+df %>%
+  summarise(n = n(), .by = c(distribuidora, id, livre, gru_tar, cnae, classe_bdgd, cep, municipio, camada, shape, lon, lat, referencia)) %>%
+  filter(n > 1)
 
 
 
-        
 
 
 
 
 
-
-
+df <- map(bdgd, \(f) {
+  ano <- str_extract(f, "20[0-9]{2}")
+  lay <- st_layers(f)$name
+  lay_filtrado <- lay %>%
+    grep("tab", ., value = TRUE, ignore.case = TRUE) %>%
+    grep("UC", ., value = TRUE, ignore.case = TRUE)
+  
+  map(lay_filtrado, \(l) {
+    st_read(f, layer = l) %>%
+      mutate(ano = ano, camada = l) %>%
+      rename_with(tolower)
+  })
+}) %>%
+  flatten() %>%
+  bind_rows() %>%
+  select(distribuidora = dist, 
+         id            = pn_con, 
+         livre         = liv, 
+         gru_tar, 
+         cnae,
+         classe_bdgd   = clas_sub,
+         cep, 
+         municipio     = mun,
+         ano,
+         camada,
+         starts_with("ene")) %>%
+  left_join(df_shape, by = "id", multiple = "first") %>%
+  select(-matches("^ene(_p|_f)")) %>%  # remove colunas ene_p e ene_f
+  pivot_longer(
+    cols = starts_with("ene"),
+    names_to = "mes",
+    values_to = "kwh"
+  ) %>%
+  mutate(
+    referencia = as.Date(paste0(ano, "-", str_extract(mes, "\\d{2}"), "-01")),
+    cnae = str_remove_all(cnae, "[^0-9]")
+  ) %>%
+  pivot_wider(
+    id_cols     = c(distribuidora, id, livre, gru_tar, cnae, classe_bdgd, cep, municipio, camada, shape, lon, lat),
+    names_from  = referencia,
+    values_from = kwh
+  )
 
 
 
